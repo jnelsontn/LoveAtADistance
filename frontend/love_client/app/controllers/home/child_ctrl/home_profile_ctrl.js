db603d21808436b253327ed9ee998004d276901b
Jordan Nelson <jnelsontn@gmail.com>
2017-06-19 11:27:19 -0500

    completed ticket 5 - profile editing

--- frontend/love_client/app/controllers/home/child_ctrl/home_profile_ctrl.js
+++ frontend/love_client/app/controllers/home/child_ctrl/home_profile_ctrl.js
@@ -1,12 +1,27 @@
 'use strict';
 
 app.controller('ProfileCtrl', function($scope, $http, 
-    apiUrl, RootFactory, profile, user_profile) {
+    $state, Upload, apiUrl, RootFactory, profile, 
+    user_profile) {
     
     console.log('ProfileCtrl Here');
 
     $scope.user = profile;
     $scope.profile = user_profile;
+
+    console.log('home_profile - user: ', $scope.user);
+    console.log('home_profile - profile: ', $scope.profile);
+    
+    let check_profile_photo = Object.keys(
+        $scope.profile.profile_photo).length;
+
+    if (check_profile_photo < 1) {
+         $scope.profile_photo = false;
+    } else {
+        $scope.profile_photo = $scope.profile.profile_photo.medium_square_crop;
+        $scope.profile_photo_full = $scope.profile.profile_photo.full_size;
+    }
+
 
     $scope.updateProfile = () => {
         // first we call the update to profile
@@ -59,6 +74,57 @@ app.controller('ProfileCtrl', function($scope, $http,
             });
         });
     };
+
+    // You can remove your relationship with them, they'll have to remove their
+    // side too...we could always force this server-side
+    $scope.disconnectPartner = () => {
+        $http({
+            method: 'DELETE',
+            url: `${apiUrl}/relationships/` + $scope.user.relationship.id,
+            headers: { 
+                'Authorization': 'Token ' + RootFactory.getToken() 
+            },
+        }).then((res) => {
+            // Update the Dom to let the user know the cancel request was successful
+            if (res.data) { 
+                $scope.response = 'Successfully Canceled Request';
+            }
+            console.log(res.data);
+            $state.go('find_partner');
+        });
+    };
+
+    $scope.uploadProfilePhoto = (file) => {
+        $scope.f = file;
+        if (file) {
+            file.upload = Upload.upload({
+                url: `${apiUrl}/profiles/` + $scope.user.id + '/',
+                method: 'PUT',
+                headers: { 
+                    'Authorization': 'Token ' + RootFactory.getToken() 
+                },
+                data: {
+                    'profile_photo': file
+                }
+            }).then((res) => { 
+                file.result = res.data;
+                $http({
+                    url: `${apiUrl}/photos/`,
+                    headers: { 'Authorization': 'Token ' + RootFactory.getToken() },
+                })
+                .then((updated_profile_photo) => {
+                    console.log(updated_profile_photo);
+                    $scope.profile_photo = $scope.profile.profile_photo.profile;
+                    $scope.f = '';
+                });
+            }, (res) => { console.log(res.status + ': ' + res.data); },
+            (evt) => {
+                file.progress = Math.min(100,
+                    parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }
+    };
+
 
 
 });
